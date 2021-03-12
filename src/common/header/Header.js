0b632291e6e18cf49af16d75c9e8a746c19cc55a
Jitesh Acharya <jitesh.acharya2400@gmail.com>
2021-03-13 01:32:16 +0530

    Added Tabs inside modal for Login button

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,25 +3,41 @@ import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
 
 class Header extends Component {
 
     constructor() {
         super();
         this.state = {
-            modalIsOpen: false
+            modalIsOpen: false,
+            value: 0
         }
     }
 
     openModalHandler = () => {
-        this.setState({modalIsOpen: true});
+        this.setState({ modalIsOpen: true });
     }
 
     closeModalHandler = () => {
-        this.setState({modalIsOpen: false});
+        this.setState({ modalIsOpen: false });
     }
 
+    tabChangeHandler = (event, value) => {
+        this.setState({ value: value });
+    }
 
     render() {
         return (
@@ -31,8 +47,11 @@ class Header extends Component {
                     <Button variant="contained" color="default" onClick={this.openModalHandler}>LOGIN</Button>
                 </div>
 
-                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
-
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler} style={customStyles}>
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="Login" />
+                        <Tab label="Register" />
+                    </Tabs>
                 </Modal>
 
             </header>
